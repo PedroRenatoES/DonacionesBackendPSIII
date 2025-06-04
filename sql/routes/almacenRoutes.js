4356d0f5f106f275bd436a5dfe93d68b831843a1
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-06-04 03:05:42 -0400

    Update almacenRoutes.js
    corregir ruta /por-almacenes

--- sql/routes/almacenRoutes.js
+++ sql/routes/almacenRoutes.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/', authenticateToken, AlmacenController.getAll);
-router.get('/por-almacenes', authenticateToken, AlmacenController.getByAlmacenes);
+router.get('/por-almacenes/:id_almacen', authenticateToken, AlmacenController.getByAlmacenes);
 router.get('/:id', authenticateToken, AlmacenController.getById);
 router.post('/', authenticateToken, AlmacenController.create);
 router.put('/:id', authenticateToken, AlmacenController.update);
