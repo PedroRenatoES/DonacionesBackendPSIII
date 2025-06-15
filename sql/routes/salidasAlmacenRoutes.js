4273a1dcc51f3368197d9d0143d3f258f65d6113
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-06-15 14:43:42 -0400

    Update salidasAlmacenRoutes.js

--- sql/routes/salidasAlmacenRoutes.js
+++ sql/routes/salidasAlmacenRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const SalidasAlmacenController = require('../controllers/salidasAlmacenController');
 
+router.get('/', SalidasAlmacenController.getAll);
+router.post('/', SalidasAlmacenController.create);
+
+module.exports = router;
