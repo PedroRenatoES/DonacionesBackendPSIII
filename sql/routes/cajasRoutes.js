0f961e859ac06057e83e77b8c05512e0f5cd739f
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-06-15 17:35:02 -0400

    Update cajasRoutes.js

--- sql/routes/cajasRoutes.js
+++ sql/routes/cajasRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const CajaController = require('../controllers/cajasController');
 
+router.get('/por-paquete/:id_paquete', CajaController.getByPaquete);
 router.get('/', CajaController.getAll);
 router.get('/:id', CajaController.getById);
-router.get('/por-paquete/:id_paquete', CajaController.getByPaquete);
 router.post('/', CajaController.create);
 router.put('/:id', CajaController.update);
 router.delete('/:id', CajaController.delete);
