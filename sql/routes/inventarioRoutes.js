ffc5b15f4ded1810105f343c0acb67c7a9cf6fe2
PedroRenatoES <pedrorenatoes@gmail.com>
2025-06-16 07:59:15 -0400

    Update inventarioRoutes.js

--- sql/routes/inventarioRoutes.js
+++ sql/routes/inventarioRoutes.js
@@ -6,6 +6,7 @@ const authenticateToken = require('../../middleware/authMiddleware');
 router.get('/ubicaciones', authenticateToken, InventarioController.getInventarioConUbicaciones);
 router.get('/stock',       InventarioController.getStockPorArticulo);
 router.get('/stock/articulo/:id', InventarioController.getStockPorArticuloPorId);
+router.get('/stock/estante/:id_estante', InventarioController.getStockPorEstanteId);
 
 
 module.exports = router;
