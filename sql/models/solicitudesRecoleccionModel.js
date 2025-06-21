f54231b80da1331da3544d6154a54818c51989a4
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-06-21 18:14:09 -0400

    Update solicitudesRecoleccionModel.js

--- sql/models/solicitudesRecoleccionModel.js
+++ sql/models/solicitudesRecoleccionModel.js
@@ -1,16 +1,20 @@
 const { sql, poolPromise } = require(require('../config').dbConnection);
 
 class SolicitudesRecoleccionModel {
-  static async create(id_donante, ubicacion, detalle_solicitud) {
+  
+  static async create(id_donante, ubicacion, detalle_solicitud, latitud, longitud, foto_url) {
     const pool = await poolPromise;
     const result = await pool.request()
       .input('id_donante', sql.Int, id_donante)
       .input('ubicacion', sql.Text, ubicacion)
       .input('detalle_solicitud', sql.Text, detalle_solicitud)
+      .input('latitud', sql.Decimal(11, 8), latitud)
+      .input('longitud', sql.Decimal(11, 8), longitud)
+      .input('foto_url', sql.VarChar, foto_url)
       .query(
-        `INSERT INTO SolicitudesRecoleccion (id_donante, Ubicacion, Detalle_solicitud)
+        `INSERT INTO SolicitudesRecoleccion (id_donante, Ubicacion, Detalle_solicitud, latitud, longitud, foto_url)
          OUTPUT INSERTED.id_solicitud
-         VALUES (@id_donante, @ubicacion, @detalle_solicitud);`
+         VALUES (@id_donante, @ubicacion, @detalle_solicitud, @latitud, @longitud, @foto_url);`
       );
     return result.recordset[0].id_solicitud;
   }
@@ -25,7 +29,10 @@ class SolicitudesRecoleccionModel {
            sr.id_donante,
            don.nombres + ' ' + don.apellido_paterno + ' ' + don.apellido_materno AS donante,
            sr.Ubicacion,
-           sr.Detalle_solicitud
+           sr.Detalle_solicitud,
+           sr.latitud,
+           sr.longitud,
+           sr.foto_url
          FROM SolicitudesRecoleccion sr
          JOIN Donantes don ON sr.id_donante = don.id_donante;`
       );
@@ -42,7 +49,10 @@ class SolicitudesRecoleccionModel {
            id_solicitud,
            id_donante,
            Ubicacion,
-           Detalle_solicitud
+           Detalle_solicitud,
+           latitud,
+           longitud,
+           foto_url
          FROM SolicitudesRecoleccion
          WHERE id_solicitud = @id_solicitud;`
       );
@@ -50,18 +60,24 @@ class SolicitudesRecoleccionModel {
   }
 
   // Actualizar solicitud existente
-  static async update(id, id_donante, ubicacion, detalle_solicitud) {
+  static async update(id, id_donante, ubicacion, detalle_solicitud, latitud, longitud, foto_url) {
     const pool = await poolPromise;
     await pool.request()
       .input('id_solicitud', sql.Int, id)
       .input('id_donante', sql.Int, id_donante)
       .input('ubicacion', sql.Text, ubicacion)
       .input('detalle_solicitud', sql.Text, detalle_solicitud)
+      .input('latitud', sql.Decimal(11, 8), latitud)
+      .input('longitud', sql.Decimal(11, 8), longitud)
+      .input('foto_url', sql.VarChar, foto_url)
       .query(
         `UPDATE SolicitudesRecoleccion
          SET id_donante = @id_donante,
              Ubicacion = @ubicacion,
-             Detalle_solicitud = @detalle_solicitud
+             Detalle_solicitud = @detalle_solicitud,
+             latitud = @latitud,
+             longitud = @longitud,
+             foto_url = @foto_url
          WHERE id_solicitud = @id_solicitud;`
       );
   }
@@ -78,4 +94,4 @@ class SolicitudesRecoleccionModel {
   }
 }
 
-module.exports = SolicitudesRecoleccionModel;   
+module.exports = SolicitudesRecoleccionModel;
