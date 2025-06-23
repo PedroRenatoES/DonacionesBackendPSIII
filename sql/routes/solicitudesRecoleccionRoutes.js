79e05703b8158b5e7682f06d82792e341a190ce3
Aquiles-Brinco2 <148920169+Aquiles-Brinco2@users.noreply.github.com>
2025-06-23 16:02:43 -0400

    Update solicitudesRecoleccionRoutes.js

--- sql/routes/solicitudesRecoleccionRoutes.js
+++ sql/routes/solicitudesRecoleccionRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SolicitudesRecoleccionController = require('../controllers/solicitudesRecollecionController');
 
+router.get('/:id_donante', SolicitudesRecoleccionController.getAllByDonante);
 router.get('/', SolicitudesRecoleccionController.getAll);
 router.get('/:id', SolicitudesRecoleccionController.getById);
 router.post('/', SolicitudesRecoleccionController.create);
